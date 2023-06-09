f8b82547be81f974f863ce1dd3eb3a1759c8dbb7
tanyaarora2 <tanya081btece21@igdtuw.ac.in>
2023-06-09 15:48:18 +0530

    added search sort filter functionality

--- Frontend/src/scenes/dashboard-social/DataSocial.js
+++ Frontend/src/scenes/dashboard-social/DataSocial.js
@@ -1,49 +1,51 @@
-import React from "react";
-import "./DataSocial.css";
-function DataSocial() {
-  const data = [
-    {
-      column1: "1",
-      column2: "BAL/6",
-      column3: "ABC",
-      column4: "10 years and 2 months",
-      column5: "Abandoned",
-    },
-    {
-      column1: "1",
-      column2: "BAL/6",
-      column3: "EFG",
-      column4: "13 years and 6 months",
-      column5: "Abandoned",
-    },
-  ];
-  return (
-    <table className="social-table">
-      <thead className="head-social">
-        <tr>
-          <th>S.NO.</th>
-          <th>CASE NUMBER</th>
-          <th>CHILD NAME</th>
-          <th>AGE</th>
-          <th>CHILD CLASSIFICATION</th>
-          <th></th>
-          {/* Add more table header columns as needed */}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            {Object.values(item).map((value, i) => (
-              <td key={i}>{value}</td>
-            ))}
-            <td>
-              <button className="details-button">Details</button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
+// import React from "react";
+// import "./DataSocial.css";
+// function DataSocial() {
+//   const data = [
+//     {
+//       sNo: "1",
+//       caseNumber: "BAL/6",
+//       childName: "ABC",
+//       age: 10,
+//       childClassification: "Abandoned",
+//     },
+//     {
+//       sNo: "1",
+//       caseNumber: "BAL/6",
+//       childName: "EFG",
+//       age: "13 years and 6 months",
+//       childClassification: "Abandoned",
+//     },
+//   ];
+//   return (
+//     <table className="social-table">
+//       <thead className="head-social">
+//         <tr>
+//           <th>S.NO.</th>
+//           <th>CASE NUMBER</th>
+//           <th>CHILD NAME</th>
+//           <th>AGE</th>
+//           <th>CHILD CLASSIFICATION</th>
+//           <th></th>
+//           {/* Add more table header columns as needed */}
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {data.map((item, index) => (
+//           <tr key={index}>
+//             <td>{item.sNo}</td>
+//             <td>{item.caseNumber}</td>
+//             <td>{item.childName}</td>
+//             <td>{item.age}</td>
+//             <td>{item.childClassification}</td>
+//             <td>
+//               <button className="details-button">Details</button>
+//             </td>
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   );
+// }
 
-export default DataSocial;
+// export default DataSocial;
